70380cb4b9adc9e089083d2aacf5c7ac02e56f5f
willykostet <56007713+willykostet@users.noreply.github.com>
2020-07-14 15:00:26 +0300

    14 июля

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,6 @@
 var button = document.querySelector(".popup-button");
 var popup = document.querySelector(".searchform");
-
+popup.classList.add("searchform-close");
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("searchform-close");
