b98e1ec79c88207bc2994e394295ff0e8c04bef9
willykostet <56007713+willykostet@users.noreply.github.com>
2020-07-15 10:49:15 +0300

    15 июля 10.49

---
+++ js/min-popup.js
@@ -0,0 +1,1 @@
+var button=document.querySelector(".popup-button");var popup=document.querySelector(".searchform");popup.classList.add("searchform-close");button.addEventListener("click",function(evt){evt.preventDefault();popup.classList.toggle("searchform-close")})
