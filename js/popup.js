952ebf910bcd18b823b7c1fa1fbacd2f90077d8e
willykostet <56007713+willykostet@users.noreply.github.com>
2020-07-15 16:31:37 +0300

    current 16.31 15 july

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,17 @@
 var button = document.querySelector(".popup-button");
 var popup = document.querySelector(".searchform");
+var arrivalDate = document.querySelector("arrival-input");
+var departureDate = document.querySelector("departure-input");
+
 popup.classList.add("searchform-close");
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("searchform-close");
+});
+
+popup.addEventListener("submit", function (evt) {
+  if (!arrivalDate.value || !departureDate.value) {
+    evt.preventDefault();
+    popup.classList.add("popup-error");
+  }
 });
