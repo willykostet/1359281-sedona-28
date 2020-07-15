ed2002381cfd241c45d9a20c1f5ae855e9ffa82f
willykostet <56007713+willykostet@users.noreply.github.com>
2020-07-15 19:56:20 +0300

    Almost

--- js/popup.js
+++ js/popup.js
@@ -8,11 +8,3 @@ button.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("searchform-close");
 });
-popup.addEventListener("sumbit", function (evt) {
-  if (!arrivalDate.value || !departureDate.value) {
-    evt.preventDefault();
-    popup.classList.remove("popup-error");
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("popup-error");
-  }
-});
